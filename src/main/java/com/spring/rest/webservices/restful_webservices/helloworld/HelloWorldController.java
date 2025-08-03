3d8d4b9f2502fa8a99a8c550f09fa87c38a6a1c2
Prabhakar <kulkarniprabhakar2000@gmail.com>
2025-08-03 18:40:35 +0900

    Basic enhancement

--- src/main/java/com/spring/rest/webservices/restful_webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/spring/rest/webservices/restful_webservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.spring.rest.webservices.restful_webservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,11 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello world bean!");
 		
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}") // {name} is path parameter to access this use path variable
+	public HelloWorldBean helloWorldPathVar(@PathVariable String name) {
+//		return new HelloWorldBean("Hello world " + name); one way of doing it
+		return new HelloWorldBean(String.format("Hello world other , %s", name));
 	}
 }
